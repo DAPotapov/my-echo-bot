eab49ef7822b22c22b77c943a54d8f06329ef8ee
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-06-25 17:19:22 +0300

    Another try

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.WARNING
 )
 
-# Set Flask application
-app = Flask(__name__)
+# # Set Flask application
+# app = Flask(__name__)
 
 
 
-@app.route("/", methods=["GET", "POST"])
-async def webhook():
-    update = Update
-    print(update)
-    await update.message.reply_text(update.message.text) 
+# @app.route("/", methods=["GET", "POST"])
+# async def webhook():
+#     update = Update
+#     print(update)
+#     await update.message.reply_text(update.message.text) 
 
 
 
@@ -28,29 +28,22 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     text = update.message.text
     await update.message.reply_text(text)
 
-<<<<<<< HEAD
 
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-PORT = int(os.getenv("PORT")) # type: ignore
-application = Application.builder().token(BOT_TOKEN).build()
-application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-# application.run_polling()
-application.run_webhook(
-    listen='0.0.0.0',
-    port=PORT,
-    secret_token=BOT_TOKEN.split(':')[1],
-    webhook_url='https://PDA.pythonanywhere.com:8443'
-)
-
-=======
 def main() -> None:
     load_dotenv()
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+    PORT = int(os.getenv("PORT"))
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    application.run_polling()
->>>>>>> parent of 8da4f0e (Starting transfer to webhook)
+    # application.run_polling()
+    application.run_webhook(
+        listen='0.0.0.0',
+        port=PORT,
+        secret_token=BOT_TOKEN.split(":")[1],
+        webhook_url="https://PDA.pythonanywhere.com:8443"
+    )
+
 
 if __name__ == "__main__":
-    app.run()
+    # app.run()
+    main()
